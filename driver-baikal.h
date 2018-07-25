8b10ddf02e22f437257405fc45a5be575dc3c3da
BuiThanhNam <itcenter4it@gmail.com>
2018-07-26 01:14:45 +0700

    Update driver-baikal.h

--- driver-baikal.h
+++ driver-baikal.h
@@ -1,52 +1,54 @@
-#ifndef __DEVICE_BAIKAL_H__
-#define __DEVICE_BAIKAL_H__
+ifndef __DEVICE_BAIKAL_H__
+define __DEVICE_BAIKAL_H__
 
-#include "miner.h"
+include "miner.h"
 
-#define BAIKAL_1751             (0x1)
-#define BAIKAL_1772             (0x2)
-#define BAIKAL_1791             (0x4)
-#define BAIKAL_ALL              (0x7)
-#define BAIKAL_TYPE             (BAIKAL_ALL)
+define BAIKAL_1751             (0x1)
+define BAIKAL_1772             (0x2)
+define BAIKAL_1791             (0x4)
+define BAIKAL_ALL              (0x7)
+define BAIKAL_TYPE             (BAIKAL_ALL)
 
-#define BAIKAL_MAXMINERS	    (5)
-#define BAIKAL_MAXUNIT          (4)
-#define BAIKAL_MAXASICS	        (16)
-#define BAIKAL_WORK_FIFO        (200)
+define BAIKAL_MAXMINERS	    (500)
+define BAIKAL_MAXUNIT          (400)
+define BAIKAL_MAXASICS	        (160000)
+define BAIKAL_WORK_FIFO        (200)
 
-#define BAIKAL_CLK_MIN  		(150)
-#define BAIKAL_CLK_MAX		    (400)
+define BAIKAL_CLK_MIN  		(150)
+define BAIKAL_CLK_MAX		    (400)
 
-#if BAIKAL_TYPE == BAIKAL_1751 // Really?
-#define BAIKAL_CLK_DEF  		(200)
-#elif BAIKAL_TYPE == BAIKAL_1791 // Really?
-#define BAIKAL_CLK_DEF  		(400)
-#else
-#define BAIKAL_CLK_DEF  		(200) // This must be changed depending on your miner type. If you have Giant N set to 200, If you have X10 set to 300 and if you have B set to 400. Its left at 200 intentionally because Giant N is stock at 200.
-#endif
+if BAIKAL_TYPE == BAIKAL_1751 // Really?
+define BAIKAL_CLK_DEF  		(200)
+if BAIKAL_TYPE == BAIKAL_1772 // Really?
+define BAIKAL_CLK_DEF  		(340)
+elif BAIKAL_TYPE == BAIKAL_1791 // Really?
+define BAIKAL_CLK_DEF  		(400)
+else
+define BAIKAL_CLK_DEF  		(330) // This must be changed depending on your miner type. If you have Giant N set to 200, If you have X10 set to 300 and if you have B set to 400. Its left at 200 intentionally because Giant N is stock at 200.
+endif
 
-#define BAIKAL_CUTOFF_TEMP      (55)
-#define BAIKAL_FANSPEED_DEF     (100)
-#define BAIKAL_FANSPEED_MAX     (100)
-#define BAIKAL_RECOVER_TEMP     (40)
+define BAIKAL_CUTOFF_TEMP      (55)
+define BAIKAL_FANSPEED_DEF     (100)
+define BAIKAL_FANSPEED_MAX     (100)
+define BAIKAL_RECOVER_TEMP     (40)
 
-#define BAIKAL_RESET		    (0x01)
-#define BAIKAL_GET_INFO	        (0x02)
-#define BAIKAL_SET_OPTION	    (0x03)
-#define BAIKAL_SEND_WORK	    (0x04)
-#define BAIKAL_GET_RESULT	    (0x05)
-#define BAIKAL_SET_ID		    (0x06)
-#define BAIKAL_SET_IDLE		    (0x07)
+define BAIKAL_RESET		    (0x01)
+define BAIKAL_GET_INFO	        (0x02)
+define BAIKAL_SET_OPTION	    (0x03)
+define BAIKAL_SEND_WORK	    (0x04)
+define BAIKAL_GET_RESULT	    (0x05)
+define BAIKAL_SET_ID		    (0x06)
+define BAIKAL_SET_IDLE		    (0x07)
 
-#define BAIKAL_MINER_TYPE_NONE  (0x00)
-#define BAIKAL_MINER_TYPE_MINI  (0x01)
-#define BAIKAL_MINER_TYPE_CUBE  (0x02)
+define BAIKAL_MINER_TYPE_NONE  (0x00)
+define BAIKAL_MINER_TYPE_MINI  (0x01)
+define BAIKAL_MINER_TYPE_CUBE  (0x02)
 
-#define BAIKAL_ENABLE_SETCLK    (0)
+define BAIKAL_ENABLE_SETCLK    (1)
 
-#define BAIKAL_CHECK_STALE      (0)
-#define BAIKAL_EN_HWE           (1)
-#define BAIKAL_CLK_FIX          (0)
+define BAIKAL_CHECK_STALE      (0)
+define BAIKAL_EN_HWE           (1)
+define BAIKAL_CLK_FIX          (1)
 
 struct asic_info {
     uint32_t nonce;
@@ -98,4 +100,4 @@ typedef struct {
 } baikal_msg;
 
 
-#endif /* __DEVICE_BAIKAL_H__ */
+endif /* __DEVICE_BAIKAL_H__ */
